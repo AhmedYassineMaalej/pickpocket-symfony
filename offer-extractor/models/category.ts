985e514df3cead46b51bcd356dbda76bf3ac7cd7
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2026-05-27 17:46:48 +0100

    replaced offer-extractor with common submodule

--- offer-extractor/models/category.ts
+++
@@ -1,8 +0,0 @@
-export class Category {
-    name: string;
-
-
-    constructor(name: string) {
-        this.name = name
-    }
-}
--- offer-extractor/repository/repository.ts
+++
@@ -1,4 +0,0 @@
-export interface Repository<T> {
-    tablename: string;
-    add(item: T): Promise<number | void>;
-}
